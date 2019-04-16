663013930b250962bafb8a6aa095181b925e3a52
luis0andres <gordopollo100@gmail.com>
2019-04-16 00:43:59 -0400

    se cre una clase de Luis Andres Cachaga Leuca

---
+++ app/src/main/java/edu/univalle/demo_b/LuisAndresCachagaLeuca.java
@@ -0,0 +1,7 @@
+package edu.univalle.demo_b;
+
+public class LuisAndresCachagaLeuca {
+    /*buenas noches inge no sabia su teniamos que editar o cambiar algo a los layaut
+    solo comente y cree la clase como el texto que nos envio o debemos de agregar algun boton o color
+     */
+}
